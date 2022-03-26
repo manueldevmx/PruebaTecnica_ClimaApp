d4467bfd6a0714b9f9737760d547c15f7a100a4e
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-03-26 09:14:33 -0600

    función convertirGrados

--- Clima_app_pruebaTecnica/src/components/Resultado.jsx
+++ Clima_app_pruebaTecnica/src/components/Resultado.jsx
@@ -6,24 +6,39 @@ const Resultado = () => {
   console.log(resultado);
 
   const { name, main, weather } = resultado;
+  const [unit, setUnit] = React.useState("C");
+  const [temp, setTemp] = React.useState(parseInt(main.temp - 273.15));
+ 
 
-  //Grados Kelvin
-  const gKelvin = 273.15;
+  const oppositeUnit = unit === "C" ? "F" : "C";
+
+  const convert = () => {
+
+    if (unit === "C") {
+      const newT = (temp) * 1.8 + 32;
+      setTemp(Math.round(newT));
+      setUnit(oppositeUnit);
+    }
+
+    if (unit === "F") {
+      const newT = ((temp - 32)*5) / 9;
+      setTemp(Math.round(newT));
+      setUnit(oppositeUnit);
+    }
+  };
 
   return (
     <div className="contenedor clima">
       <h2>El Clima de {name} es: </h2>
     
-      <p>{parseInt(main.temp - gKelvin)}</p>
+      <p>{temp}{unit}</p>
       <p>{weather[0].main}</p>
-      <div className="temp_min_max">
-        <p>Mín: {parseInt(main.temp_min - gKelvin)}</p>
-        <p>Max: {parseInt(main.temp_max - gKelvin)}</p>
+      
+      <button  
+        className="btnChange"
+        onClick={convert}>Cambiar escala</button>
       </div>
-      <div>
-     
-      </div>
-    </div>
+   
   );
 };
 
